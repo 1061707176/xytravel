89125a01c13d4d6848a124cd349a59b9b6074b29
曾文 <214138467@qq.com>
2019-09-14 16:02:18 +0800

    酒店详情

---
+++ components/hotel/hotelCrumbs.vue
@@ -0,0 +1,46 @@
+<template>
+<!-- 这个是设置酒店面包屑的 -->
+  <div class="hotelCrumbs" >
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        
+    <el-breadcrumb-item :to="{ path: '/' }">{{data[0].big_cate}}</el-breadcrumb-item>
+    <el-breadcrumb-item class="jiazu">{{data[0].real_city}}{{data[0].big_cate}}</el-breadcrumb-item>
+    <el-breadcrumb-item>{{data[0].name}}</el-breadcrumb-item>
+</el-breadcrumb>   
+  </div>
+</template>
+
+<script>
+export default {
+props:{
+  // data表示组件可以接收的属性
+  data:{
+    // type不能修改，用于声明属性的类型
+    type:Array,
+    // 如果调用组件不传值，采用default的默认值
+    default:[]
+  }
+},
+methods:{
+
+},
+mounted(){
+  // console.log(this.data)
+  // //  console.log(this.data,123465789)
+  // setTimeout(()=>{
+  //   this.data
+  //   // console.log(this.data,123)
+  // },1)
+},
+
+}
+</script>
+
+<style lang="less" scoped>
+.hotelCrumbs{
+  .jiazu{
+    font-weight: 700;
+    color: #303133
+  }
+}
+</style>
---
+++ components/hotel/hotelPrice.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="hotelPrice">
+  <el-row :gutter="20" class="across headlines">
+  <el-col :span="5" >
+      <div class="headline">价格来源</div>
+      </el-col>
+  <el-col :span="11">
+      <div class="headline centered">低价房型</div>
+  </el-col>
+  <el-col :span="8"><div class="headline centered">最低价格/每晚</div>
+  </el-col>
+  
+</el-row>
+<el-row :gutter="20" class="across hotelSource" v-for="(item,index) in data[0].products" :key="index">
+    <a href="http://baidu.com">
+  <el-col :span="5"><div class="details">{{item.name}}</div></el-col>
+  <el-col :span="11"><div class="details centered ">{{item.bestType}}</div></el-col>
+  <el-col :span="8"><div class="details centered dye"><span>￥</span><span>{{item.price}}</span>起<span></span></div></el-col>
+    </a>
+</el-row>
+</div>
+</template>
+
+<script>
+export default {
+props:{
+    data:{
+        type:Array,
+        // 如果调用组件不传值，采用default的默认值
+        default:[]
+    }
+},
+mounted(){
+    setTimeout(()=>{
+    this.data
+    // console.log(this.data,123123123)
+  },1)
+}
+}
+</script>
+
+<style lang="less" scoped>
+.hotelPrice{
+    margin: 40px 0;
+    .headlines{
+        padding: 12px 0;
+        border-bottom: 1px solid #ebeef5;
+        .headline{
+            color:#909399;
+            font-size: 14px;
+            font-weight:600;
+            
+        }
+        .centered{
+            text-align: center;
+        }
+    };
+    .hotelSource{
+        border-bottom: 1px solid #ebeef5;
+        &:hover{
+            background-color: #f5f7fa;
+        }
+        .details{
+            padding: 12px 0;
+            
+            font-size: 14px;
+            color:#606266;
+            span{
+                color:#f90;
+                padding-right: 3px;
+                font-size: 16px;
+            }
+        }
+        .centered{
+            text-align: center;
+        }
+    }
+}
+</style>
